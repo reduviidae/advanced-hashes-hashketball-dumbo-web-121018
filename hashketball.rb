858f929a7d69b973bc7ac1b81682d2231eb823f5
Wil Cook <wil.cook.is@gmail.com>
2018-11-26 21:57:49 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -7,53 +7,53 @@ def game_hash
       :colors => ["Black", "White"],
       :players => {
         "Alan Anderson" => {
-          :number => "0",
-          :shoe => "16",
+          :number => 0,
+          :shoe => 16,
           :points => 22,
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "3",
-          :blocks => "1",
-          :slam_dunks => "1"
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
         },
         "Reggie Evans" => {
-          :number => "30",
-          :shoe => "14",
+          :number => 30,
+          :shoe => 14,
           :points => 12,
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "12",
-          :blocks => "12",
-          :slam_dunks => "7"
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
         },
         "Brook Lopez" => {
-          :number => "11",
-          :shoe => "17",
+          :number => 11,
+          :shoe => 17,
           :points => 17,
-          :rebounds => "19",
-          :assists => "10",
-          :steals => "3",
-          :blocks => "1",
-          :slam_dunks => "15"
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
         },
         "Mason Plumlee" => {
-          :number => "1",
-          :shoe => "19",
+          :number => 1,
+          :shoe => 19,
           :points => 26,
-          :rebounds => "12",
-          :assists => "6",
-          :steals => "3",
-          :blocks => "8",
-          :slam_dunks => "5"},
+          :rebounds => 12,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5},
         "Jason Terry" => {
-          :number => "31",
-          :shoe => "15",
+          :number => 31,
+          :shoe => 15,
           :points => 19,
-          :rebounds => "2",
-          :assists => "2",
-          :steals => "4",
-          :blocks => "11",
-          :slam_dunks => "1"
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
         }
       }
     },
@@ -62,54 +62,54 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => {
         "Jeff Adrien" => {
-          :number => "4",
-          :shoe => "18",
+          :number => 4,
+          :shoe => 18,
           :points => 10,
-          :rebounds => "1",
-          :assists => "1",
-          :steals => "2",
-          :blocks => "7",
-          :slam_dunks => "2"
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         },
-        "Bismack Biyombo" => {
-          :number => "0",
-          :shoe => "16",
+        "Bismak Biyombo" => {
+          :number => 0,
+          :shoe => 16,
           :points => 12,
-          :rebounds => "4",
-          :assists => "7",
-          :steals => "7",
-          :blocks => "15",
-          :slam_dunks => "10"
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 7,
+          :blocks => 15,
+          :slam_dunks => 10
         },
         "DeSagna Diop" => {
-          :number => "2",
-          :shoe => "14",
+          :number => 2,
+          :shoe => 14,
           :points => 24,
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "4",
-          :blocks => "5",
-          :slam_dunks => "5"
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
         },
         "Ben Gordon" => {
-          :number => "8",
-          :shoe => "15",
+          :number => 8,
+          :shoe => 15,
           :points => 33,
-          :rebounds => "3",
-          :assists => "2",
-          :steals => "1",
-          :blocks => "1",
-          :slam_dunks => "0"
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
         },
         "Brendan Haywood" => {
-          :number => "33",
-          :shoe => "15",
+          :number => 33,
+          :shoe => 15,
           :points => 6,
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "22",
-          :blocks => "5",
-          :slam_dunks => "12"
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 22,
+          :blocks => 5,
+          :slam_dunks => 12
         }
       }
     }
@@ -117,12 +117,55 @@ def game_hash
 end
 
 def num_points_scored(name)
-  binding.pry
-  game_hash.first
+  # binding.pry
+  game_hash.map do |team, info|
+    info[:players].map do |player, stats|
+      # binding.pry
+      if name == player 
+        # binding.pry
+        return stats[:points]
+      end
+    end
+  end
 end
 
+def shoe_size(name)
+  game_hash.map do |team, info|
+    info[:players].map do |player, stats|
+      # binding.pry
+      if name == player 
+        # binding.pry
+        return stats[:shoe]
+      end
+    end
+  end
+end
 
+def team_colors(team_name)
+  game_hash.map do |team, info|
+    # binding.pry
+    if team_name == info[:team_name]
+      return info[:colors]
+    end
+  end
+end
 
+def team_names
+  game_hash.map do |team, info|
+    info[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  game_hash.map do |team, info|
+    # binding.pry
+    if team_name == info[:team_name]
+      info[:players].map do |player, stats|
+        stats[:number]
+      end
+    end
+  end
+end
 
 
 
